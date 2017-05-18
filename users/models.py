30c203edc38c40568448e0ea1c2db1f1764d2982
Hane Liu <hane0131@gmail.com>
2017-05-18 22:49:10 +0800

    Initial user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    pass
+
+class User(AbstractBaseUser, PermissionsMixin):
+    pass
