1afc35f46b5469ce05394dd7c31cb2f3c48846e4
Hane Liu <hane0131@gmail.com>
2017-05-26 22:42:51 +0800

    Add signup view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render, HttpResponse
+from django.utils.translation import ugettext
 
-# Create your views here.
+from .forms import UserCreationForm
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            return HttpResponse('Successed!')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
