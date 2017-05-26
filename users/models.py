6e5cd0e3af7f5f5926323c46dd1fa14112469b62
Hane Liu <hane0131@gmail.com>
2017-05-26 14:37:26 +0800

    Set base user model field

--- users/models.py
+++ users/models.py
@@ -2,9 +2,18 @@ from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin,
 )
 from django.db import models
+from django.utils import timezone
 
 class UserManager(BaseUserManager):
     pass
 
 class User(AbstractBaseUser, PermissionsMixin):
-    pass
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    name = models.CharField(max_length=100)
+    verified = models.BooleanField(default=False)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
