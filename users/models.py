ee9bf306fae569222d77eb0726517a9cee331c34
Hane Liu <hane0131@gmail.com>
2017-05-26 15:47:38 +0800

    Set method for creating user

--- users/models.py
+++ users/models.py
@@ -5,7 +5,33 @@ from django.db import models
 from django.utils import timezone
 
 class UserManager(BaseUserManager):
-    pass
+    """Custom manager for User
+    """
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        """Create and save an EmailUser with the given email and password.
+        :param str email: user email
+        :param str password: user password
+        :param bool is_staff: whether user staff or not
+        :param bool is_superuser: whether user admin or not
+        :return custom_user.models.EmailUser user: user
+        :raise ValueError: email is not set
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        last_login = extra_fields.pop('last_login', now)
+        date_joined = extra_fields.pop('date_joined', now)
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            last_login=last_login,
+            date_joined=date_joined,
+            **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True, db_index=True)
