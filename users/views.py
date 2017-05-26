4a4f9c7271fafa611d274365739e0f7081303701
Hane Liu <hane0131@gmail.com>
2017-05-26 23:04:08 +0800

    Add login view and templates

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import login
+from django.contrib.auth.views import (
+    login as base_login
+)
 from django.shortcuts import redirect, render, HttpResponse
 from django.utils.translation import ugettext
 
-from .forms import UserCreationForm
+from .forms import AuthenticationForm, UserCreationForm
 
 
 def user_signup(request):
@@ -16,3 +18,6 @@ def user_signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
+
+def user_login(request):
+    return base_login(request, authentication_form=AuthenticationForm, template_name='users/login.html')
