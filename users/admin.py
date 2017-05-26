b337e7dca393bb87f667fcf629fa3078cd20574e
Hane Liu <hane0131@gmail.com>
2017-05-26 21:14:32 +0800

    Set form and add_form

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import ugettext_lazy as _
 
 from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -44,3 +45,9 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'is_staff')
+    search_fields = ('email',)
+    ordering = ('email',)
