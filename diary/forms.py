e7b6dbccfe62d13f12a9f2e4d83914c54ebd60e0
IssaL <aa10240tw@gmail.com>
2017-06-11 00:56:13 +0800

    solve pic captain modify upload-media-display.html modify textera auto resize

--- diary/forms.py
+++ diary/forms.py
@@ -18,7 +18,7 @@ class MediaForm(forms.ModelForm):
         fields = ['title', 'description', 'img']
         widgets ={
             'title': forms.TextInput(attrs={'placeholder':'給圖片取個名字嘛？', 'class':'form-control', 'style':'display:inline;width:100%;'}),
-            'description': forms.Textarea(attrs={'placeholder':'說一說這張圖片吧？', 'rows':'2','class': 'form-control', 'style':'width:100%;display:inline-block;'}),
+            'description': forms.Textarea(attrs={'id':'textarea','placeholder':'說一說這張圖片吧？', 'rows':'1','class': 'form-control', 'style':'width:100%;display:inline-block;resize:none;overflow:hidden;'}),
             'img':forms.FileInput(attrs={'class':'custom-file-input', 'style':'width:100%;display:inline-block;'})
         }
 
