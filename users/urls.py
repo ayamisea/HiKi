2fed696e58cac8b66d5da1a4d1a5430c9f23f9b3
Irene Lai <lucylai0102@gmail.com>
2017-06-11 23:42:38 +0800

    add navbar

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^signup/$', views.user_signup, name='signup'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^$', views.home),
+    url(r'^$', views.home,name='account'),
 ]
