00be185087af5dbcf957b5ac276bc01a7d7d3063
Irene Lai <lucylai0102@gmail.com>
2017-06-11 18:45:17 +0800

    modify page style

--- diary/urls.py
+++ diary/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^unit_test/$', views.unit_test), # unit_test.html
-    url(r'^$', views.display), # display.html
+    url(r'^$', views.display,name='diary_display'), # display.html
     url(r'^detail/(?P<pk>\d+)/$', views.detail, name='detail'), # detail.html
     url(r'^map/$', views.map), # display-map.html
     url(r'^media/$', views.media), # display-media.html
@@ -14,5 +14,5 @@ urlpatterns = [
     url(r'^media-upload/$', views.media_upload), # upload-media.html
     url(r'^media-upload-show/$', views.media_upload_show), # upload-media-display.html
     url(r'^edit/(?P<pk>\d+)/$', views.edit,name='edit'), # edit.html
-    url(r'^search/$', views.search), # search.html
+    url(r'^search/$', views.search,name='search'), # search.html
 ]
