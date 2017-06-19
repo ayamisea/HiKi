5f28f3369a799c1f154a400411b02fd1f0d0f4a8
Hane Liu <hane0131@gmail.com>
2017-06-19 11:05:17 +0800

    Add user verify view

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.user_signup, name='signup'),
+    url(r'^verify/(?P<verification_key>[-:\w]+)/$',
+        views.user_verify, name='user_verify'),
 
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', auth.logout, {'next_page': '/accounts/'}, name='logout'),
