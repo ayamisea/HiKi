3a8833abce5a332054e11d74bb50246e182cf396
IssaL <aa10240tw@gmail.com>
2017-06-19 21:05:55 +0800

    adjust tally/models.py indentation

--- tally/models.py
+++ tally/models.py
@@ -9,16 +9,6 @@ User =  get_user_model()
 # Create your models here.
 
 class Tally(models.Model) :
-	TYPE_CHOICES = ( 
-		('食物', '食物'), 
-		('玩樂' , '玩樂'),
-		('交通', '交通'),
-		('其他', '其他'),
-	)
-	PAY_CHOICES = (
-		('收入', '收入'),
-		('支出', '支出'),
-	)
 	PAY_CHOICES = (
 		('收入', (
 			('打工', '打工'),
@@ -32,17 +22,17 @@ class Tally(models.Model) :
 			('其他支出', '其他支出'),
 		) ),
 	)
-
-	class MoneyTypes(DjangoChoices) :
-		income = ChoiceItem()
 	userID = models.ForeignKey(User,blank=True,null=True)
 	date   = models.DateField(default=timezone.now)
 	type   = models.CharField(max_length = 20, choices=PAY_CHOICES)
 	subtype = models.CharField(max_length = 20)
 	price  = models.DecimalField(max_digits=10,decimal_places=0,default=Decimal('0'))
 	notes  = models.CharField(max_length=200)
+	
 	def __str__(self):
 		return str(self.id)
+	class MoneyTypes(DjangoChoices) :
+		income = ChoiceItem()
 	class Meta:
 		ordering = ['date']
 
