fbbd8a5fafc81758a0571f23c0f3c43ae49af755
IssaL <aa10240tw@gmail.com>
2017-06-20 00:04:05 +0800

    tally type change

--- tally/templatetags/sort.py
+++ tally/templatetags/sort.py
@@ -2,6 +2,13 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='get_type')
+def get_type(value,choices):
+	if value in [ k for k,v in dict(choices)['收入']]:
+  		return 'income'
+	if value in [ k for k,v in dict(choices)['支出']]:
+  		return 'expense'
+
 @register.filter(name='datelist')
 def datelist(value):
 	dates = sorted(set([ t.date for t in value ]),reverse=True)	
