d12508c204452c6dc3076a92ff7a7b6f9e90f3a2
IssaL <aa10240tw@gmail.com>
2017-06-20 04:10:32 +0800

    new .datepicker into calendar.js

--- staticfiles/js/calendar.js
+++ staticfiles/js/calendar.js
@@ -1,6 +1,18 @@
-//日曆
+//日曆 id
 $( function() {
 	$( "#datepicker" ).datepicker({
+		dateFormat: 'yy-mm-dd',
+		minDate: new Date(2014, 6 - 1, 5),
+		maxDate: "0",
+		changeMonth: true,
+		changeYear: true,
+		showButtonPanel: true,
+	});
+});
+
+//日曆 class
+$( function() {
+	$( ".datepicker" ).datepicker({
 		dateFormat: 'yy-mm-dd',
 		minDate: new Date(2014, 6 - 1, 5),
 		maxDate: "0",
