bfc9c1747ddb2a2cae621fb5fc1de7196a3c10ad
Hane Liu <hane0131@gmail.com>
2017-06-20 21:23:01 +0800

    Add new pattern

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.display, name='gallery'), # display-media.html
+    url(r'^new/', views.new, name='gallery_new'),
     url(r'^media-upload/$', views.media_upload), # upload-media.html
     url(r'^media-upload-show/$', views.media_upload_show), # upload-media-display.html
 ]
