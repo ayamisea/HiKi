d38e5576a842c7871533476694d07dafce76d392
Hane Liu <hane0131@gmail.com>
2017-06-20 19:54:26 +0800

    Update gallery model

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Media
+from .models import Image, DiaryImage
 
-admin.site.register(Media)
+admin.site.register(Image)
+admin.site.register(DiaryImage)
--- gallery/models.py
+++ gallery/models.py
@@ -1,47 +1,80 @@
+from django.utils import timezone
+
 from django.db import models
 
 from diary.models import Diary
 from users.models import User
 
-
-class Image(models.Model):
-    user = models.ForeignKey(User,
-        on_delete=models.CASCADE,
-        default=0,
-    )
+class Media(models.Model):
+    """Base Model for media.
+    """
     title = models.CharField(
         max_length=20, null=True, blank=True,
     )
     description = models.CharField(
         max_length=80, null=True, blank=True,
     )
-    img = models.ImageField(upload_to='user_media/')
+    """pub_date = models.DateTimeField(
+        default=timezone.now,
+        editable=False,
+    )"""
 
-class BlogImage(models.Model):
-    title = models.CharField(
-        max_length=20, null=True, blank=True,
-    )
-    description = models.CharField(
-        max_length=80, null=True, blank=True,
-    )
-    img = models.FileField(upload_to='user_media/')
-    diary = models.ForeignKey(Diary,
+
+def image_upload_to(instance, filename):
+    return 'image/{pk}/{date}-{filename}'.format(
+        pk=instance.user.pk,
+        date=str(timezone.now().date()),
+        filename=filename,
+        )
+
+class Image(Media):
+    """User upload images.
+    """
+    user = models.ForeignKey(
+        User,
         on_delete=models.CASCADE,
         default=0,
     )
+    img = models.ImageField(upload_to=image_upload_to)
 
     def __str__(self):
-        return str(self.pk)
+        return str(self.img)
 
+    def delete(self, *args, **kwargs):
+        super(Image, self).delete(*args, **kwargs)
+        storage, path = self.img.storage, self.img.path
+        storage.delete(path)
+
+
+def diary_image_upload_to(instance, filename):
+    return 'image/{pk}/diary/{date}-{filename}'.format(
+        pk=instance.diary.userID.pk,
+        date=str(timezone.now().date()),
+        filename=filename,
+        )
+
+class DiaryImage(Media):
+    """Images in diary.
+    """
+    diary = models.ForeignKey(
+        Diary,
+        on_delete=models.CASCADE,
+        default=0,
+    )
+    img = models.FileField(upload_to=diary_image_upload_to)
+
+    def __str__(self):
+        return str(self.img)
+
+    def delete(self, *args, **kwargs):
+        super(DiaryImage, self).delete(*args, **kwargs)
+        storage, path = self.img.storage, self.img.path
+        storage.delete(path)
+    """
     def type(self):
         import os
         name,ext = os.path.splitext(self.img.name)
         ext = ext.lower()
         if ext =='.jpg' or ext =='.jpeg' or ext == '.png' or ext=='.gif':
             return 'img'
-        return 'video'
-
-    def delete(self, *args, **kwargs):
-        storage, path = self.img.storage, self.img.path
-        super(Media, self).delete(*args, **kwargs)
-        storage.delete(path)
+        return 'video'"""
