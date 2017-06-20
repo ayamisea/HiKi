235102c7595f9b2974e1876e8fe8a5468a33e02b
Hane Liu <hane0131@gmail.com>
2017-06-20 17:22:48 +0800

    Add new model in gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 
 from diary.models import Diary
+from users.models import User
 
 
-class Media(models.Model):
-    title = models.CharField(max_length=20,null=True,blank=True)
-    description = models.CharField(max_length=80,null=True,blank=True)
-    img = models.FileField(upload_to = 'user_media/')
-    diary = models.ForeignKey(Diary,on_delete=models.CASCADE,default=0)
+class Image(models.Model):
+    user = models.ForeignKey(User,
+        on_delete=models.CASCADE,
+        default=0,
+    )
+    title = models.CharField(
+        max_length=20, null=True, blank=True,
+    )
+    description = models.CharField(
+        max_length=80, null=True, blank=True,
+    )
+    img = models.ImageField(upload_to='user_media/')
+
+class BlogImage(models.Model):
+    title = models.CharField(
+        max_length=20, null=True, blank=True,
+    )
+    description = models.CharField(
+        max_length=80, null=True, blank=True,
+    )
+    img = models.FileField(upload_to='user_media/')
+    diary = models.ForeignKey(Diary,
+        on_delete=models.CASCADE,
+        default=0,
+    )
 
     def __str__(self):
         return str(self.pk)
