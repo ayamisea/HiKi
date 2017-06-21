b485e44b6783ee1a1d057ed00473bead2e80eb81
Hane Liu <hane0131@gmail.com>
2017-06-21 12:06:53 +0800

    Reformat

--- diary/forms.py
+++ diary/forms.py
@@ -10,23 +10,27 @@ class DiaryForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(
                 attrs={
-                    'placeholder':'標題（必填）',
-                    'class':'form-control m-1',
-                }),
+                    'placeholder': '標題（必填）',
+                    'class': 'form-control m-1',
+                }
+            ),
             'date': forms.DateInput(
                 attrs={
-                    'id':'datepicker',
-                    'placeholder':'2017-06-01（必填）',
+                    'id': 'datepicker',
+                    'placeholder': '2017-06-01（必填）',
                     'class': 'form-control m-1',
-                }),
+                }
+            ),
             'content': forms.Textarea(
                 attrs={
-                    'id':'textarea',
+                    'id': 'textarea',
                     'class': 'form-control m-1',
-                    'style':'overflow: hidden;',
-                }),
+                    'style': 'overflow: hidden;',
+                }
+            ),
             'post_type':forms.Select(
                 attrs={
-                    'class':'form-control m-1',
-                })
+                    'class': 'form-control m-1',
+                }
+            ),
         }
